d935877576ba54e79c0ae1c5500d16361e0e815f
Vit <privit@rambler.ru>
2023-05-29 20:56:28 +0300

    в начале был код...

---
+++ src/main/java/ru/skypro/homework/spring_data_jpa/HwSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.homework.spring_data_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwSpringDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwSpringDataJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/skypro/homework/spring_data_jpa/HwSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.homework.spring_data_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwSpringDataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
